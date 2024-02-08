e76a76c325851dc65395f1f6feaef12d6d72a3bc
Suraj Kumar Verma <surajkumarverma662@gmail.com>
2024-02-08 15:29:54 +0530

    loaded full E-waste project in django

---
+++ E_waste_locator/E_waste_locator/urls.py
@@ -0,0 +1,38 @@
+"""
+URL configuration for E_waste_locator project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from E_waste_locator import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("",views.home),
+    path("signup/",views.signupF),
+    path("signup_ewaste/",views.signupC),
+    path("signuploginv/",views.signuplogV),
+    path("login_ewaste/",views.loginE),
+    path("loginc/",views.loginC),
+    path("recycle_dashboard/",views.recycleD),
+    path("status/",views.status),
+    path("orderc/",views.orderC),
+    path("home/",views.Home),
+    path("form/",views.Form),
+    path("fsubmit/",views.Submit),
+    path("information/",views.Info),
+    path("nearest/",views.Near),
+    path("credit/",views.Credit),
+    path("voldashboard/",views.Vdash),
+]
---
+++ E_waste_locator/E_waste_locator/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+ return render(request,"index.html")
+def signupF(request):
+ return render(request,"signup.html")
+def signupC(request):
+ return render(request,"signup_ewaste.html")
+def signuplogV(request):
+ return render(request,"Vsignuplogin.html")
+def loginE(request):
+ return render(request,"login_ewaste.html")
+def loginC(request):
+ return render(request,"loginc.html")
+def recycleD(request):
+ return render(request,"recycle_dashboard.html")
+def status(request):
+ return render(request,"status.html")
+def orderC(request):
+ return render(request,"orderconfirm.html")
+def Home(request):
+ return render(request,"home.html")
+def Form(request):
+ return render(request,"form.html")
+def Submit(request):
+ return render(request,"submit.html")
+def Info(request):
+ return render(request,"information.html")
+def Near(request):
+ return render(request,"nearest1.html")
+def Credit(request):
+ return render(request,"credit.html")
+def Vdash(request):
+ return render(request,"voldashboard.html")
+ 
